bca57474a29f8e6f3217247907e90d4061cc09c5
arlindofernandes <arlindo_fernandes22@hotmail.com>
2023-03-09 14:53:13 -0300

    add mentoria e curso

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    Curso curso1 = new Curso();
+    curso1.setTitulo("java");
+    curso1.setDescricao("java baciso");
+    curso1.setCargaHoraria(8);
+
+    Mentoria mentoria= new Mentoria();
+     mentoria.setTitulo("mentoria java");
+     mentoria.setDescricao("mentoria java basico");
+     mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+
+        System.out.println(mentoria);
     }
 }
